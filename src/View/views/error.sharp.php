46e419e22ec291b45e2a7d2638dea8eeba4fe980
meteguerlek <meteguerlek@gmail.com>
2018-07-29 23:58:21 +0200

    missing files

--- src/View/views/error.sharp.php
+++ src/View/views/error.sharp.php
@@ -1,9 +1,13 @@
-@extends('homepage.layout')
-
-@section('title', 'Page not found')
-
-@section('content')
-
-    <h1 style="text-align: center; padding-top: 400px ; ">Page not found.</h1>
-
-@endsection
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+</head>
+<body>
+<h1 style="text-align: center; padding-top: 400px ; ">Page not found.</h1>
+</body>
+</html>
